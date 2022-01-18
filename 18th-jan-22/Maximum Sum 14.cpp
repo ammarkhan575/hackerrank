440dddf63acd6919d7600bad7e85fc793547de37
mohd ammar <72297085+ammarkhan575@users.noreply.github.com>
2022-01-17 22:01:51 -0800

    Create Maximum Sum 14.cpp

---
+++ 18th-jan-22/Maximum Sum 14.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    int sum=0,maxsum=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        sum = sum + ar[i];
+        maxsum=max(sum,maxsum);
+        if(sum<0)
+            sum=0;
+        
+    }
+    cout<<maxsum<<endl;
+    return 0;
+}
